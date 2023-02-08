05ade5cc8d43e761e2ec4a21e3df353c4a254a40
alexsystemas <ing-alejandro@outlook.es>
2023-02-07 22:27:50 -0600

    Agregar archivos de proyecto.

---
+++ CONEXION/CONEXIONMAESTRA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUNTO_DE_VENTA.CONEXION
+{
+    class CONEXIONMAESTRA
+    {  // creamos la conexion hacia la BD
+        public static string conexion = @"Data source=DESKTOP-VBGGJBO;Initial Catalog=BASE_PUNTO_VENTA;Integrated Security=True";
+
+    }
+}
